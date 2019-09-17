2af539c3753325f9e06ba13a1dafe5e18262901a
Jennivvo <54864141+Jennivvo@users.noreply.github.com>
2019-09-17 10:11:09 +0200

    Add files via upload

---
+++ Flip the class.js
@@ -0,0 +1,46 @@
+//type: String
+var name = "Henrik";
+
+//type: Number
+var age = 18;
+
+//type: Boolean
+var bool = true;
+
+//type: Empty
+var top = null ;
+
+//type: Array
+var t =[1,2,3];
+
+//type: Object
+Person(name,age){
+};
+
+//Operators + Expression
+var a = 3;
+var b = 4;
+var c = a + b;
+
+//Bindings
+let d = 5;
+
+//Functions
+for (n=0; n<=100; n++){console.log(n)
+}
+//Loops
+
+//Statements
+if (bool==true){console.log("true is always true")
+}
+
+//Scoping med forskellige scope
+let e = 18;
+var f = 6;
+
+//Call Stack = debugging
+
+//Recursion: en funktion der kalder på sig selv
+function(s){function(s);
+}
+
